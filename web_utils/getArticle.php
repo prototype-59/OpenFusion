2ad0746e982a4de29f85285806cc10f2acddb8d5
aleksar <aleksandar.radovanovic@kaust.edu.sa>
2020-07-15 15:22:31 +0300

    graph updated

--- web_utils/getArticle.php
+++ web_utils/getArticle.php
@@ -52,6 +52,7 @@ $data = array (
 header("Content-Type: application/json;charset=utf-8");
 print json_encode($data,JSON_PRETTY_PRINT);
 
+
 //-----------------------------------------------------------------------------
 // reverse array of arrays sort based on the 3rd index
 //-----------------------------------------------------------------------------
